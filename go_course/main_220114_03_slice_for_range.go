61ec3a0b2e8149435cffc7b5a6b28a495ebf536a
williamszk <wllmszk@gmail.com>
2022-01-14 18:46:47 -0800

    go course

---
+++ go_course/main_220114_03_slice_for_range.go
@@ -0,0 +1,32 @@
+// https://www.youtube.com/watch?v=l6O8HWaoy_w&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=61&ab_channel=AprendaGo
+
+package main
+
+import "fmt"
+
+func main() {
+	mySlice := []string{"banana", "maça", "jaca"}
+
+	for index, value := range mySlice {
+		fmt.Println("In index", index, "we got", value)
+	}
+
+	// if we give a new value this way bad things will happen
+	// mySlice[3] = "melancia"
+	mySlice = append(mySlice, "melancia")
+	for index, value := range mySlice {
+		fmt.Println("In index", index, "we got", value)
+	}
+	// it is not good to expand a slice by using element assignment, instead we use append()
+
+	mySlice2 := []int{23, 24, 54}
+	totalValue := 0
+	for _, value := range mySlice2 {
+		totalValue += value
+	}
+
+	fmt.Println("The total value is:", totalValue)
+}
+
+// next class
+// https://www.youtube.com/watch?v=G0rxcnojV_U&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=62&ab_channel=AprendaGo

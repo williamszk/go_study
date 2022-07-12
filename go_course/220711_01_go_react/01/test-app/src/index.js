6f8b04d43ecec4a8bf4b2f2e4a2820625bc26bf0
williamszk <wllmszk@gmail.com>
2022-07-11 19:20:12 -0700

    go react course

---
+++ go_course/220711_01_go_react/01/test-app/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+
+// import React, { Component } from "react";
+// class App extends Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Hello, world!</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+
+function App() {
+	return (
+		<div>
+			<h1>Hello, world!</h1>
+		</div>
+	);
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ go_course/220711_01_go_react/readme.md
@@ -0,0 +1,20 @@
+
+
+https://www.udemy.com/course/working-with-react-and-go-golang/learn/lecture/24190294?start=0#overview
+
+Login local postgres installation
+postgres
+
+5432
+
+What is the default root username in postgres?
+postgres
+
+
+How to connect to psql with another user?
+
+psql -U postgres 
+postgres
+
+
+

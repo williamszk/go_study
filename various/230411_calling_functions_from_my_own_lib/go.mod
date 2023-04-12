26410f2ca5184abe568e39d98c8d00cc2e48c8ac
williamszk <wllmszk@gmail.com>
2023-04-12 08:57:59 -0300

    experiment calling my own lib

---
+++ various/230411_calling_functions_from_my_own_lib/go.mod
@@ -0,0 +1,5 @@
+module calling_my_own_lib
+
+go 1.19
+
+// replace a_lib_in_go => github.com/williamszk/a_lib_in_go
---
+++ various/230411_calling_functions_from_my_own_lib/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/williamszk/a_lib_in_go"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	a_lib_in_go.SayHi()
+}

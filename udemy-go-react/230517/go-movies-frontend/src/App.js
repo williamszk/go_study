aca8c3ed5be507b9c74d9b8d19a5b7b28044068c
williamszk <wllmszk@gmail.com>
2023-07-20 00:06:50 +0000

    go react study, react-router-dom

--- udemy-go-react/230517/go-movies-frontend/src/App.js
+++ udemy-go-react/230517/go-movies-frontend/src/App.js
@@ -1,4 +1,4 @@
-import Home from './components/Home';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 	return (
@@ -19,10 +19,13 @@ function App() {
 				<div className="col-md-2">
 					<nav>
 						<div className="list-group">
-							<a href="#!" className="list-group-item list-group-item-action">
+							<a href="/" className="list-group-item list-group-item-action">
 								Home
 							</a>
-							<a href="#!" className="list-group-item list-group-item-action">
+							<a
+								href="/movies"
+								className="list-group-item list-group-item-action"
+							>
 								Movies
 							</a>
 							<a href="#!" className="list-group-item list-group-item-action">
@@ -41,7 +44,7 @@ function App() {
 					</nav>
 				</div>
 				<div className="col-md-10">
-					<Home />
+					<Outlet />
 				</div>
 			</div>
 		</div>
--- udemy-go-react/230517/go-movies-frontend/src/index.js
+++ udemy-go-react/230517/go-movies-frontend/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Home from './components/Home';
+import Movies from './components/Movies';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: '/movies', element: <Movies /> },
+		],
 	},
 ]);
 
